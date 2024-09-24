ae6e053af53c73d6485159f35dea7453044f2870
Denselle0311 <144681965+Denselle0311@users.noreply.github.com>
2024-09-24 21:12:47 +0800

    feat: Show modal to create book

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,81 @@
-// book
-// title - author - pages - status
+const modal = document.querySelector('.modal');
+const bgDark = document.querySelector('.bg-dark');
+const addBookBtn = document.querySelector('.add-book-btn');
+const closeDialogBtn = document.querySelector('.close-btn');
+const currentPages = document.querySelector('#current-pages');
+const saveBtn = document.querySelector('.save-book-btn');
+
+class Book {
+    constructor(
+        title,
+        author,
+        pages,
+        language,
+        status,
+    ) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.language = language;
+        this.status = status;
+        this.id = crypto.randomUUID();
+    }
+}
+
+class LibraryClass {
+    constructor() {
+        this.books = [];
+    }
+
+    addBook(newBook) {
+        this.books.push(newBook);
+    }
+
+    removeBook(toRemoveBook) {
+        this.books = this.books.filter(b => b.id != toRemoveBook.id);
+    }
+
+    getBook(findBook) {
+        return this.books.find(b => b.id == findBook.id);
+    }
+
+    isInTheLibrary(findBook) {
+        return this.books.find(b => b.id == findBook.id);
+    }
+}
+
+function openModal() {
+    modal?.classList.add('active')
+    bgDark?.classList.add('active');
+}
+
+function closeModal() {
+    modal?.classList.remove('active')
+    bgDark?.classList.remove('active');
+}
+
+const Library = new LibraryClass();
+
+const isDone = document.querySelector('#is-done');
+
+// eventListener
+
+addBookBtn?.addEventListener('click', () => {
+    openModal();
+});
+
+saveBtn?.addEventListener('click', ()=> {
+    closeModal();
+});
+
+closeDialogBtn?.addEventListener('click', () => { 
+    closeModal();
+});
+
+bgDark?.addEventListener('click', () => {
+    closeModal();
+})
+
+isDone?.addEventListener('click', e => {
+    currentPages.disabled = e.target.checked;
+});
